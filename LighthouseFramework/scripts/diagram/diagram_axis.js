998d963d8d3c7de3e97bb24de9cea18877e77ff8
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-27 20:06:16 +0200

    working

---
+++ LighthouseFramework/scripts/diagram/diagram_axis.js
@@ -0,0 +1,116 @@
+﻿class PresentAxisOperation
+{
+    constructor( show )
+    {
+        this.show = show;
+
+    }
+
+    get Show()
+    {
+        return this.show;
+    }
+
+    __drawLine( ctx, sP, eP )
+    {
+        ctx.moveTo( sP.X, sP.Y );
+        ctx.lineTo( eP.X, eP.Y );
+    }
+}
+
+
+class PresentXAxisOperation
+    extends PresentAxisOperation
+{
+    constructor()
+    {
+        super( true );
+
+    }
+
+    draw( canvasContext, screenSize )
+    {
+        if( this.Show )
+        {
+            const pS = new PositionVector(0, calculateOffsetY(0, screenSize.Y ) );
+            const pE = new PositionVector( screenSize.X, calculateOffsetY(0, screenSize.Y ) );
+
+            canvasContext.beginPath();
+
+            this.__drawLine( canvasContext, pS, pE );
+
+            canvasContext.closePath();
+            canvasContext.stroke();
+        }
+
+    }
+
+}
+
+
+class PresentYAxisOperation
+    extends PresentAxisOperation
+{
+    constructor()
+    {
+        super( true );
+
+    }
+
+    draw( canvasContext, screenSize )
+    {
+        if( this.Show )
+        {
+            const pS = new PositionVector(0, calculateOffsetY(0, screenSize.Y));
+            const pE = new PositionVector(0, calculateOffsetY(screenSize.Y, screenSize.Y) );
+
+            canvasContext.beginPath();
+
+            this.__drawLine( canvasContext, pS, pE );
+
+            canvasContext.closePath();
+            canvasContext.stroke();
+        }
+    }
+}
+
+class PresentAxis
+{
+    constructor()
+    {
+        this.yAxis = new PresentYAxisOperation();
+        this.xAxis = new PresentXAxisOperation();
+
+        this.screenSize = null;
+
+        this.showAxis = true;
+    }
+
+    get Screen()
+    {
+        return this.screenSize;
+    }
+
+    set Screen( v )
+    {
+        this.screenSize = v;
+    }
+
+    draw( canvasContext )
+    {
+        canvasContext.lineWidth = 4;
+
+        if( this.ShowAxis )
+        {
+            this.xAxis.draw( canvasContext, this.Screen );
+            this.yAxis.draw( canvasContext, this.Screen );
+        }
+
+        canvasContext.lineWidth = 1;
+    }
+
+    get ShowAxis()
+    {
+        return this.showAxis;
+    }
+}
---
+++ LighthouseFramework/scripts/math/vector.js
@@ -0,0 +1,133 @@
+﻿/**
+ *
+ */
+class BaseVector
+{
+    constructor( x, y )
+    {
+        this.X = x;
+        this.Y = y ;
+    }
+
+    // Accessors
+        // Getters
+    get X()
+    {
+        return this.x;
+    }
+
+    get Y()
+    {
+        return this.y;
+    }
+
+        // Setters
+    set X( x )
+    {
+        this.x = x;
+    }
+
+    set Y( y )
+    {
+        this.y = y;
+    }
+
+    flip()
+    {
+        return this.flipX(), this.flipY();
+    }
+
+    flipX()
+    {
+        return -this.X;
+    }
+
+    flipY()
+    {
+        return -this.Y;
+    }
+
+    logState()
+    {
+        console.log( { 'X' : this.X, 'Y' : this.Y } )
+    }
+}
+
+
+/**
+ * A vector, which has a length and a direction, that it is moving in, x & y indicates in which direction
+ */
+class Vector
+    extends BaseVector
+{
+    constructor( x, y )
+    {
+        super( x, y );
+    }
+
+    assign( x, y )
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    length()
+    {
+        const px2 = Math.pow( this.X, 2 );
+        const py2 = Math.pow( this.Y, 2 );
+
+        return Math.sqrt( ( px2 + py2 ) );
+    }
+}
+
+
+/**
+ * Position Vector, indicates a given position in a 2 dimensional space.
+ */
+class PositionVector
+    extends BaseVector
+{
+    constructor( x, y )
+    {
+        super( x, y );
+    }
+
+}
+
+
+//
+let z = null;
+
+
+/**
+ *
+ */
+class ZeroPositionVector
+{
+    constructor()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    get X()
+    {
+        return this.x;
+    }
+
+    get Y()
+    {
+        return this.y;
+    }
+
+
+    static generate()
+    {
+        if( z == null )
+        {
+            z = new ZeroPositionVector();
+        }
+
+        return z;
+    }
+}
